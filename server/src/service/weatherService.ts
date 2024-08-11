840a765456c342a10f8683eb1bdc530d5870aa6c
Kagen Smith <kagensmith@Smiths-iMac.local>
2024-08-11 14:56:10 -0600

    added more methods to weatherService.ts

--- server/src/service/weatherService.ts
+++ server/src/service/weatherService.ts
@@ -2,8 +2,9 @@ import dotenv from 'dotenv';
 dotenv.config();
 import fs from 'fs';
 
-const baseURL: string = process.env.API_BASE_URL! || 'https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={d6e209a4939923e24d5d9b282dd40ca9}'
+const baseURL: string = process.env.API_BASE_URL! || `https://api.openweathermap.org`
 const apiKey: string =  process.env.API_KEY! || "d6e209a4939923e24d5d9b282dd40ca9";
+const name = " ";
 // TODO: Define an interface for the Coordinates object
 interface Coordinates {
   lat: number;
@@ -40,12 +41,29 @@ class WeatherService {
     this.name = name;
   }
   // TODO: Create fetchLocationData method
-  // private async fetchLocationData(query: string) {}
+  private async fetchLocationData(query: string): Promise<Coordinates> {
+    const response = await fetch(`${this.baseURL}/geo/1.0/direct?q=${query}&limit=1&appid=${this.apiKey}`);
+    const data = await response.json();
+    if (data.length === 0) {
+      throw new Error('No location found');
+    }
+    const locationData = data[0];
+    return {
+      lat: locationData.lat,
+      lon: locationData.lon,
+    };
+  }
+  
 
   // TODO: Create destructureLocationData method
-  // private destructureLocationData(locationData: Coordinates): Coordinates {}
+  private async destructureLocationData(locationData: Coordinates): Promise<Coordinates> {
+    const { lat, lon } = locationData;
+    return { lat, lon };
+  }  
   // TODO: Create buildGeocodeQuery method
-  // private buildGeocodeQuery(): string {}
+  private async buildGeocodeQuery(): Promise<string> {
+    
+  }
   // TODO: Create buildWeatherQuery method
   // private buildWeatherQuery(coordinates: Coordinates): string {}
   // TODO: Create fetchAndDestructureLocationData method
